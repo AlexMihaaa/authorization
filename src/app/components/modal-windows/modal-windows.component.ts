1f627856efec902c3fb62aafc30a39fe2a0b0feb
AlexMihaaa <a.mihaylichenko@nv.dunice.net>
2023-02-06 12:14:15 +0300

    create components

---
+++ src/app/components/modal-windows/modal-windows.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core'
+
+@Component({
+  selector: 'app-modal-windows',
+  templateUrl: './modal-windows.component.html',
+  styleUrls: ['./modal-windows.component.scss'],
+  standalone: true,
+})
+export class ModalWindowsComponent {}
